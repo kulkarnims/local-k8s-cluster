50300395f4ce92b7b0a62177b0cfe59a5fc0ae25
Mahesh Kulkarni <mahesh.kulkarni02@gmail.com>
2018-12-25 19:42:00 -0500

    renamed the example environmet file to local environment, generated a cookbook for kubernetes master

---
+++ k8s-cluster/cookbooks/k8s-master/README.md
@@ -0,0 +1,4 @@
+# k8s-master
+
+TODO: Enter the cookbook description here.
+
---
+++ k8s-cluster/cookbooks/k8s-master/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: k8s-master
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
