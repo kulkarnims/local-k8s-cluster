d0b952668b08e1db42ce35f546c57f9bc32d0f63
Mahesh Kulkarni <mahesh.kulkarni02@gmail.com>
2018-12-25 20:51:30 -0500

    Added ruby Gemfile to track gem dependencies like chef-provisioning, added machine resource for k8s-master recipe

--- k8s-cluster/cookbooks/k8s-master/recipes/default.rb
+++ k8s-cluster/cookbooks/k8s-master/recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+vagrant_box 'ubuntu/xenial64' do
+    url 'http://cloud-images.ubuntu.com/xenial/20181217/xenial-server-cloudimg-amd64-vagrant.box'
+end
